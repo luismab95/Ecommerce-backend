b418ef601bc7c5edc626fb7cd9d6a3b090054a07
Luis Barragan <luismab95@gmail.com>
2025-12-08 19:09:05 -0500

    feat: Add API controllers for authentication, categories, images, orders, products, and users.

---
+++ Ecommerce.Api/Controllers/AuthController.cs
@@ -0,0 +1,188 @@
+﻿using Ecommerce.Api.Filters;
+using Ecommerce.Domain.DTOs.Auth;
+using Ecommerce.Domain.DTOs.General;
+using Ecommerce.Application.UseCases.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Api.Controllers;
+
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly AuthUseCases _authUseCases;
+
+    public AuthController(AuthUseCases authUseCases)
+    {
+        _authUseCases = authUseCases;
+    }
+
+
+    [HttpPost("sign-up")]
+    public async Task<IActionResult> SignUp([FromBody] SignUpRequest request)
+    {
+        try
+        {
+            var result = await _authUseCases.RegisterUserAsync(request);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+    [HttpPost("sign-in")]
+    public async Task<IActionResult> SignIn([FromBody] SignInRequest request)
+    {
+        try
+        {
+            request.Ip = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "";
+            request.DeviceInfo = HttpContext.Request.Headers.UserAgent.ToString();
+
+            var result = await _authUseCases.LoginUserAsync(request);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<IActionResult> RefreshToken()
+    {
+        try
+        {
+            if (!HttpContext.Request.Headers.TryGetValue("Authorization", out var authHeader))
+            {
+                return Unauthorized(new GeneralResponse { Message = "Token no presente en la solicitud." });
+            }
+
+            var parts = authHeader.ToString().Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2 || !parts[0].Equals("Bearer", StringComparison.OrdinalIgnoreCase))
+            {
+                return Unauthorized(new GeneralResponse { Message = "Formato de token inválido." });
+            }
+
+            var token = parts[1];
+            var result = await _authUseCases.RefreshTokenAsync(token);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+
+    [HttpPost("sign-out")]
+    [Authorize]
+    [ServiceFilter(typeof(PostAuthorizeFilter))]
+    public async Task<IActionResult> Logout()
+    {
+        try
+        {
+            var rawAuth = HttpContext.Request.Headers.Authorization.ToString();
+            var token = rawAuth.Replace("Bearer ", "").Trim();
+
+            var result = await _authUseCases.LogoutUserAsync(token);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+    [HttpPost("forgot-password")]
+    public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordRequest request)
+    {
+        try
+        {
+            var result = await _authUseCases.ForgotPasswordAsync(request);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" + ex.ToString() });
+        }
+    }
+
+    [HttpPost("reset-password")]
+    [Authorize]
+    public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest request)
+    {
+        try
+        {
+            var rawAuth = HttpContext.Request.Headers.Authorization.ToString();
+            var token = rawAuth.Replace("Bearer ", "").Trim();
+            request.Token = token;
+
+
+            var result = await _authUseCases.ResetPasswordAsync(request);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+}
---
+++ Ecommerce.Api/Controllers/ProductController.cs
@@ -0,0 +1,150 @@
+﻿using Ecommerce.Api.Filters;
+using Ecommerce.Domain.DTOs.General;
+using Ecommerce.Domain.DTOs.Products;
+using Ecommerce.Application.UseCases.Products;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Api.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductController : ControllerBase
+{
+    private readonly ProductUseCases _productUseCases;
+
+    public ProductController(ProductUseCases productUseCases)
+    {
+        _productUseCases = productUseCases;
+    }
+
+
+    [HttpGet("")]
+    public async Task<IActionResult> GetProducts([FromQuery] GetProductsWithFiltersRequest request)
+    {
+        try
+        {
+            var result = await _productUseCases.GetProductsAsync(request);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor " });
+        }
+    }
+
+    [HttpGet("{productId}")]
+    public async Task<IActionResult> GetProductById(int productId)
+    {
+        try
+        {
+            var result = await _productUseCases.GetProductByIdAsync(productId);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+
+    [HttpPost("")]
+    [Authorize]
+    [ServiceFilter(typeof(PostAuthorizeFilter))]
+    [ServiceFilter(typeof(PostAuthorizeRoleFilter))]
+    public async Task<IActionResult> AddProduct([FromBody] ProductRequest request)
+    {
+        try
+        {
+            var result = await _productUseCases.AddProductAsync(request);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+    [HttpPut("{productId}")]
+    [Authorize]
+    [ServiceFilter(typeof(PostAuthorizeFilter))]
+    [ServiceFilter(typeof(PostAuthorizeRoleFilter))]
+    public async Task<IActionResult> UpdateProduct(int productId, [FromBody] ProductRequest request)
+    {
+        try
+        {
+            var result = await _productUseCases.UpdateProductAsync(productId, request);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+
+    [HttpDelete("{productId}")]
+    [Authorize]
+    [ServiceFilter(typeof(PostAuthorizeFilter))]
+    [ServiceFilter(typeof(PostAuthorizeRoleFilter))]
+    public async Task<IActionResult> DeleteProduct(int productId)
+    {
+        try
+        {
+            var result = await _productUseCases.DeleteProductAsync(productId);
+
+            return Ok(new GeneralResponse
+            {
+                Data = result,
+                Message = "Proceso realizado con éxito."
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new GeneralResponse { Message = ex.Message });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new GeneralResponse { Message = "Error interno del servidor" });
+        }
+    }
+
+
+
+}
